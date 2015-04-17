60ffe114bf2ecb3782cdbb86d5cec99a29025fb5
John Schulz <johnschulz75@gmail.com>
2015-04-17 15:51:28 -0500

    Initial seed - Testing User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ActiveRecord::Base
 
 	has_many :cars
 	has_many :notifications
-	
+
+	validates :email, uniqueness: true, presence: true, format: { with: /[A-z](\w|[.])*@[A-z]+[.][A-z]{2,}/}
+	validates :password_digest, presence: true
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# User seeds
+john = { email:"johnschulz75@gmail.com", password_digest:"1234567890" }
+dan = { email:"hanson.e.daniel@gmail.com", password_digest:"1234567890" }
+test1 = { email:"a@a.ab", password_digest:"1234567890" }
+test2 = { email:"should_fail", password_digest:"1234567890" }
+test3 = { email:"should_fail@should_fail", password_digest:"1234567890" }
+test4 = { email:"should_fail@should_fail.s", password_digest:"1234567890" }
+
+User.create(john)
+User.create(dan)
+User.create(test1)
+User.create(test2)
+User.create(test3)
+User.create(test4)
