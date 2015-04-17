9ae5ffed1cc3e40fca479ce9fc2daef71d95af15
John Schulz <johnschulz75@gmail.com>
2015-04-17 16:06:10 -0500

    implementing has_secure_password

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # User seeds
-john = { email:"johnschulz75@gmail.com", password_digest:"1234567890" }
-dan = { email:"hanson.e.daniel@gmail.com", password_digest:"1234567890" }
-test1 = { email:"a@a.ab", password_digest:"1234567890" }
-test2 = { email:"should_fail", password_digest:"1234567890" }
-test3 = { email:"should_fail@should_fail", password_digest:"1234567890" }
-test4 = { email:"should_fail@should_fail.s", password_digest:"1234567890" }
 
-User.create(john)
-User.create(dan)
-User.create(test1)
-User.create(test2)
-User.create(test3)
-User.create(test4)
+users = [ john = { email:"johnschulz75@gmail.com", password_digest:"1234567890" },
+					dan = { email:"hanson.e.daniel@gmail.com", password_digest:"1234567890" },
+					test1 = { email:"a@a.ab", password_digest:"1234567890" },
+					test2 = { email:"should_fail", password_digest:"1234567890" },
+					test3 = { email:"should_fail@should_fail", password_digest:"1234567890" },
+					test4 = { email:"should_fail@should_fail.s", password_digest:"1234567890" } ]
+
+users.each_with_index do |user, i|
+	temp = User.new(user)
+	puts "User \##{i + 1} created" if temp.save
+end
