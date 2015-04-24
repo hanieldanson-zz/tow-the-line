facaeca9adf012afa77a32a8f2f7617f88ef011f
John Schulz <johnschulz75@gmail.com>
2015-04-24 15:31:30 -0500

    User model set for MVP

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+	def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+	def logged_in?
+		 current_user && current_user.id == session[:user_id]
+	end
+
 end
