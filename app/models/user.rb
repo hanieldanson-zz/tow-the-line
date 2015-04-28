290e77d6a7f3b20d5204eed098be37de4cdb4e84
John Schulz <johnschulz75@gmail.com>
2015-04-28 13:59:22 -0500

    Improving password validation and adding seeds for testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 	has_many :cars
 	has_many :notifications
 
-	validates :email, uniqueness: true, presence: true, format: { with: /[A-Z a-z](\w|[.])*@[A-z]+[.][A-z]{2,}/}
+	validates :email, uniqueness: true, presence: true, format: { with: /\A[A-Z a-z](\w|[.])*@[A-z]+[.][A-z]{2,}/}
 	validates :password, presence: true
 
 end
